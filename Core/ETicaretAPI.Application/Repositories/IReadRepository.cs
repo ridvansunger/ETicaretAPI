0445ca808ac55113ceb0683587afcda21afe4770
Rıdvan Sünger <92206160+ridvansunger@users.noreply.github.com>
2023-01-21 17:15:03 +0300

    Initial commit

---
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IReadRepository<T>:IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll();
+        //şarta göre veri getirme
+        IQueryable<T> GetWhere(Expression<Func<T,bool>>method);
+        Task<T> GetSingleAsync(Expression<Func<T,bool>>method);
+        Task<T> GetByIdAsync(string id);
+    }
+}
---
+++ Infrastracture/ETicaretAPI.Persistence/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    static class Configuration
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                ConfigurationManager configurationManager = new();
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API")); //SetBasePath veAddJsonFile fonjsonları Json libden geldi.
+                configurationManager.AddJsonFile("appsettings.json");
+
+                return configurationManager.GetConnectionString("PostgreSQL");
+            }
+        }
+    }
+}
