7644b7093c9eec6792012319fec16e127a5583b2
Rıdvan Sünger <92206160+ridvansunger@users.noreply.github.com>
2023-01-22 00:38:21 +0300

    7

--- Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -11,10 +11,10 @@ namespace ETicaretAPI.Application.Repositories
 {
     public interface IReadRepository<T>:IRepository<T> where T : BaseEntity
     {
-        IQueryable<T> GetAll();
+        IQueryable<T> GetAll(bool tracking=true);
         //şarta göre veri getirme
-        IQueryable<T> GetWhere(Expression<Func<T,bool>>method);
-        Task<T> GetSingleAsync(Expression<Func<T,bool>>method);
-        Task<T> GetByIdAsync(string id);
+        IQueryable<T> GetWhere(Expression<Func<T,bool>>method, bool tracking = true);
+        Task<T> GetSingleAsync(Expression<Func<T,bool>>method, bool tracking = true);
+        Task<T> GetByIdAsync(string id, bool tracking = true);
     }
 }
--- Presentation/ETicaretAPI.API/Controllers/ProductController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,13 +21,16 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new() {Id=Guid.NewGuid(),Name="Product 1",Price=100,CreatedDate=DateTime.UtcNow,Stock=10},
-                new() {Id=Guid.NewGuid(),Name="Product 2",Price=200,CreatedDate=DateTime.UtcNow,Stock=20},
-                new() {Id=Guid.NewGuid(),Name="Product 3",Price=300,CreatedDate=DateTime.UtcNow,Stock=130}
-            });
-            await _productWriteRepository.SaveAsync();
+           
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            Product product=await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
         }
     }
+
+
 }
